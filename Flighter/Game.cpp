233577324c4f92d2487eec3d303040f2d457ea45
njufyz <632041613@qq.com>
2017-05-25 01:32:37 +0800

    Plane可以跟随鼠标移动

--- Flighter/Game.cpp
+++ Flighter/Game.cpp
@@ -1,10 +1,11 @@
 #include "Game.h"
 #include"MyPlane.h"
-
+#include"Enemy.h"
 Game::Game()
 {
 	screen = new Screen;
 	myplane = new MyPlane(0.0, 0.0, 0.0);
+	enemy.push_back(new Enemy(0.0, 0.0, 0.0));
 }
 
 Game::~Game()
